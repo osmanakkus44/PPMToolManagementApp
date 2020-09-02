e61c3d5defcba138bc589bed194aebb761b00988
osmanakkus44 <osmanakkus44@gmail.com>
2020-09-02 21:10:28 +0300

    object validatin v1

--- src/main/java/com/enos/ppmtool/domain/Project.java
+++ src/main/java/com/enos/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.enos.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,14 +12,26 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required.")
     private String projectName;
+
+    @NotBlank(message = "Project identifier is required.")
+    @Size(min = 4, max = 5, message = "Please use 4-5 characters.")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required.")
     private String description;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     public Project() {
