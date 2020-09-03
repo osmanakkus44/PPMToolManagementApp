6d171565d227620e500d0e6ee37d1c84de2429cf
osmanakkus44 <osmanakkus44@gmail.com>
2020-09-04 00:11:38 +0300

    error beautify

--- src/main/java/com/enos/ppmtool/controllers/ProjectController.java
+++ src/main/java/com/enos/ppmtool/controllers/ProjectController.java
@@ -5,9 +5,12 @@ import com.enos.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,7 +25,13 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
-            return new ResponseEntity<>("Invalid Object", HttpStatus.BAD_REQUEST);
+            HashMap<String, String>  errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project project1 = projectService.saveOrUpdateProject(project);
